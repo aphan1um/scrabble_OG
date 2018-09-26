55e869a2d707eab7be0539f2107d5d9bfff3bc7e
Andy Phan <aphan1@student.unimelb.edu.au>
2018-09-26 12:53:39 +1000

    Prepare basic network model.

--- scrabble_client/src/client/ScrabbleBoard.java
+++ scrabble_client/src/client/ScrabbleBoard.java
@@ -1,3 +1,5 @@
+package client;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
--- scrabble_client/src/server/Player.java
+++ scrabble_client/src/server/Player.java
@@ -4,26 +4,14 @@ import java.net.InetAddress;
 
 public class Player {
     private String id;
-    private String ip_address = "NONE";
+    private InetAddress ip_address;
+    private int port;
 
     public Player(String id) {
         this.id = id;
     }
 
-    public Player(String id, String ip_address) {
-        this.id = id;
-        this.ip_address = ip_address;
-    }
-
     public String getID() {
         return id;
-    }
-
-    public String getIPAddress() {
-        return ip_address;
-    }
-
-    public String toString() {
-        return getID();
     }
 }
