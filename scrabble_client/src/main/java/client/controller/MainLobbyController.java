9511fb96eb98a11deffdaa02a1d0cfd6c8650da2
Murphy <murphysunx@gmail.com>
2018-10-14 18:03:08 +1100

    jump to joinLobby

--- scrabble_client/src/main/java/client/controller/MainLobbyController.java
+++ scrabble_client/src/main/java/client/controller/MainLobbyController.java
@@ -4,9 +4,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -41,5 +43,19 @@ public class MainLobbyController implements Initializable {
         AnchorPane.setLeftAnchor(node, 0.0);
         AnchorPane.setRightAnchor(node, 0.0);
         paneChat.getChildren().add(node);
+
+        btnJoin.setOnAction(e -> {
+            FXMLLoader loader1 = new FXMLLoader(getClass().getResource("/JoinLobby.fxml"));
+            JoinLobbyController joinLobbyController = new JoinLobbyController();
+
+            loader1.setController(joinLobbyController);
+
+            Stage joinStage = new Stage();
+            try {
+                joinStage.setScene(new Scene(loader1.load()));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
     }
 }
