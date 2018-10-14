4c402c0341d0bc7927026af78c2ed3eb24bafa17
Andy Phan <aphan1@student.unimelb.edu.au>
2018-10-14 12:27:17 +1100

    Quick fix with lobby owner leaving lobby

--- scrabble_client/src/main/java/core/messageType/MSGAgentChanged.java
+++ scrabble_client/src/main/java/core/messageType/MSGAgentChanged.java
@@ -9,16 +9,18 @@ import java.util.Collection;
 public class MSGAgentChanged implements Message {
     private Collection<Agent> agents;
     private NewStatus status;
+    private boolean hostLeft;
 
     public enum NewStatus {
         DISCONNECTED,
         JOINED
     }
 
-    public MSGAgentChanged(NewStatus status, Agent... agents) {
+    public MSGAgentChanged(NewStatus status, boolean hostLeft, Agent... agents) {
         super();
         this.status = status;
         this.agents = Arrays.asList(agents);
+        this.hostLeft = hostLeft;
     }
 
     public MSGAgentChanged(NewStatus status, Collection<Agent> agents) {
@@ -33,5 +35,9 @@ public class MSGAgentChanged implements Message {
 
     public NewStatus getStatus() {
         return status;
+    }
+
+    public boolean hasHostLeft() {
+        return hostLeft;
     }
 }
