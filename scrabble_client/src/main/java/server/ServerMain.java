67b4927cb304ba1842b8a17055e3e2fb9772e193
Andy Phan <aphan1@student.unimelb.edu.au>
2018-10-15 11:45:20 +1100

    fix

--- scrabble_client/src/main/java/server/ServerMain.java
+++ scrabble_client/src/main/java/server/ServerMain.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 public class ServerMain {
 
     public static void main(String[] args) throws IOException {
-        if (args.length != 1 || !args[0].matches("^[0-9]+$")) {
-            System.out.println("Please input only a port number as an argument.");
+        if (!args[0].matches("^[0-9]+$")) {
+            System.out.println("Please input a numerical port number as an argument.");
             System.exit(-1);
         }
 
